c40b56118ced05affaf7feeeea29c78ff26582be
Mn0dsew <dima.nesterov.96@bk.ru>
2020-11-17 14:00:11 +0500

    update pushka minigame

--- Assets/01 Game/C# scripts/CastleMinigame/CastleMinigameController.cs
+++ Assets/01 Game/C# scripts/CastleMinigame/CastleMinigameController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CastleMinigameController : MonoBehaviour
 {
     public CastleController CastleController;
     public PushkaController PushkaController;
+    public FinishTriggerController FinishTriggerController;
     public Transform Start, End;
+    public CinemachineVirtualCamera Camera;
 }
---
+++ Assets/01 Game/C# scripts/CastleMinigame/FinishTriggerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTriggerController : MonoBehaviour
+{
+    public Action OnPlayerEnter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnPlayerEnter?.Invoke();
+        }
+    }
+}
