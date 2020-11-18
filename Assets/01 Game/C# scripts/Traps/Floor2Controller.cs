bb1a5d5b101dd45ae53dbd1661a40653a0e53a8d
Mn0dsew <dima.nesterov.96@bk.ru>
2020-11-18 13:51:02 +0500

    added new floor prefab

---
+++ Assets/01 Game/C# scripts/Traps/Floor2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor2Controller : MonoBehaviour
+{
+    private const int enemyTypesCount = 1;
+
+    [SerializeField] private List<GameObject > enemyTypes = new List<GameObject>();
+    void Start()
+    {
+        SpawnRandomEnemyType();
+    }
+
+    private void SpawnRandomEnemyType()
+    {
+        for (int i = 0; i < enemyTypesCount; i++)
+        {
+            var randomType = enemyTypes[Random.Range(0, enemyTypes.Count)];
+            randomType.SetActive(true);
+        }
+    }
+}
