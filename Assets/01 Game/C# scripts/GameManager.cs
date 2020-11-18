b6f387347d31d4c3124ee6a931013cb30163739c
Mn0dsew <dima.nesterov.96@bk.ru>
2020-11-18 17:54:00 +0500

    update game manager

--- Assets/01 Game/C# scripts/GameManager.cs
+++ Assets/01 Game/C# scripts/GameManager.cs
@@ -9,11 +9,21 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     public CameraController CameraController;
     public LevelController LevelController;
-    //public Transform Player;
+
+    //fuck
+    public GameObject SceneVariables;
+    
+    //fuck
     public FlowMachine Player;
 
     private void Awake()
     {
         if (Instance == null) Instance = this;
+    }
+
+    public List<GameObject> GetPlayerLifeScoreObjectList()
+    {
+        var list = Variables.Scene(gameObject).Get<List<GameObject>>("PlayerLifeScoreObjectList");
+        return list;
     }
 }
